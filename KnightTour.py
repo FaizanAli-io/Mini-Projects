d09f992a38e2616e77b78ffcb8cd3f9f8928429c
Faizan Ali <fali.abdulali@gmail.com>
2020-07-04 17:39:28 +0500

    patch-GUI

--- KnightTour.py
+++ KnightTour.py
@@ -1,6 +1,34 @@
-import pygame, sys, time
+import pygame, sys
+from tkinter import *
 
-n = 6#int(input('Dimensions of chessboard please: '))
+screen = Tk()
+
+screen.geometry('250x200')
+screen.title("Knights Tour GUI")
+
+Label(screen, text="The  Knights'  Tour", width="300", height='3', font='comicsans').pack()
+Label(screen, text='Size of the grid:').place(x=10, y=60)
+Label(screen, text='Starting Coordinates    X:').place(x=10, y=100)
+Label(screen, text='Y:').place(x=190, y=100)
+
+n, startx, starty = StringVar(), StringVar(), StringVar()
+Entry(screen, textvariable=n).place(x=100, y=62, width='40')
+Entry(screen, textvariable=startx).place(x=150, y=102, width='30')
+Entry(screen, textvariable=starty).place(x=210, y=102, width='30')
+
+def get_form_info():
+    global n, startx, starty, start
+    try:
+        n = int(n.get())
+        start = (int(starty.get()), int(startx.get()))
+        screen.destroy()
+    except:
+        print("invalid entry")
+
+Button(screen, text='Generate Tour', bg="grey", width='20', height='2', command=get_form_info).place(x=50, y=140)
+
+screen.mainloop()
+
 size = 400//n
 
 def safeMove(x, y, board):
@@ -17,10 +45,10 @@ def solveKT(n):
     global coords
     coords = []
     board = [[-1 for i in range(n)]for i in range(n)]
-    board[0][0] = 0
-    coords.append((0, 0))
+    board[start[0]][start[1]] = 0
+    coords.append(start)
     pos = 1
-    if(not solveKTUtil(n, board, 0, 0, pos)):
+    if(not solveKTUtil(n, board, start[0], start[1], pos)):
         print("Solution does not exist")
     else:
         return coords
@@ -67,6 +95,7 @@ class Grid:
         for line in self.boxes:
             for box in line:
                 pygame.draw.rect(screen, green, box.position, 2)
+        pygame.draw.circle(screen, red, self.coords[0], 10)
         font = pygame.font.SysFont('comicsans', 30)
         for i in range(n):
             loclet = (self.boxes[n-1][i].position.centerx, self.boxes[n-1][i].position.centery+self.bs)
@@ -76,15 +105,16 @@ class Grid:
             text = font.render(str(n-i), True, white)
             screen.blit(text, locnum)
 
-
-    
     def generate_path(self, coords):
         self.coords = []
         for coordinate in coords:
             self.coords.append(self.boxes[coordinate[0]][coordinate[1]].position.center)
-    
+        font = pygame.font.SysFont('comicsans', 15)
+        self.textlines = [font.render(f"{i+1}.  {'ABCDEFGHIJ'[coord[1]]}{n-(coord[0])}", True, red) for i, coord in enumerate(coords)]
+        
     def draw_path(self):
         global solved
+        
         if solved == False:
             count = 2
             while count < len(self.coords)+1:
@@ -95,6 +125,8 @@ class Grid:
                 for coord in self.coords[:count]:
                     pygame.draw.circle(screen, red, coord, 10)
                 pygame.draw.lines(screen, blue, False, self.coords[:count], 4)
+                for i in range(len(self.textlines[:count-1])):
+                    screen.blit(self.textlines[i], (700, 50+(15*i)))
 
                 count += 1
                 pygame.display.flip()
@@ -104,6 +136,8 @@ class Grid:
             for coord in self.coords:
                 pygame.draw.circle(screen, blue, coord, 10)
             pygame.draw.lines(screen, blue, False, self.coords, 4)
+            for i in range(len(self.textlines)):
+                screen.blit(self.textlines[i], (700, 50+(15*i)))
             
 
 mygrid = Grid(n, n, size)
